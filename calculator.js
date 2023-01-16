47017937d02692e1a1f48188ac7c6a1425e19f4d
ajani35 <ajanidawson69@gmail.com>
2023-01-16 17:07:50 -0600

    Add files via upload
    This is my JavaScript Calculator

---
+++ calculator.js
@@ -0,0 +1,41 @@
+const readlineSync = require('readline-sync');
+
+var num1 = readlineSync.questionInt("Enter your first number: ");
+var num2 = readlineSync.questionInt("Enter your second number: ");
+var interOperation = readlineSync.question("Do you want to add, sub, div, or multiply? ");
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+function subtractNumbers(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+}
+
+function divideNumbers(num1, num2) {
+    return num1 / num2;
+}
+
+function nodeCalculator(num1, num2, interOperation) {
+    if (interOperation == "add"){
+        console.log("The first number is: " + num1 + "" +  " plus the second number: " + num2 + "" +  " gives you the answer of: " + "" + addNumbers(num1, num2));
+    }
+    else if (interOperation == "sub"){
+        console.log("The first number is: " + num1 + "" +  " minus the second number: " + num2 + "" +  " gives you the answer of: " + "" + subtractNumbers(num1, num2));
+    }
+    else if (interOperation == "multiply"){
+        console.log("The first number is: " + num1 + "" +  " multiply the second number: " + num2 + "" +  " gives you the answer of: " + "" + multiplyNumbers(num1, num2));
+    }
+    else if (interOperation == "div") {
+        console.log("The first number is: " + num1 + "" +  " divided by the second number: " + num2 + "" +  " gives you the answer of: " + "" + divideNumbers(num1, num2));
+    }
+
+}
+nodeCalculator(num1, num2, interOperation);
+
+/* credit for assignment: Valencia Parr
+https://portfolium.com/entry/javascript-calculator-using-node-and-readline-sync*/
