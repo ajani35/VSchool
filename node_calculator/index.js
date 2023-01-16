d54dfc5eccf680eadc732a8136a3f06bf1bd912b
ajani35 <ajanidawson69@gmail.com>
2023-01-16 16:28:55 -0600

    Delete node_calculator directory

--- node_calculator/index.js
+++
@@ -1,9 +0,0 @@
-const readline = require("readline-sync")
-
-const operator = readlinesync.keyIn("do you want to (a)dd, (s)ubract, (d)ivide, or (m)ultiply?")
-
-if (operator === 'add') {
-    console.log("Enter a number: ")
-    const num1 = parseInt()
-    const num2 = parstInt()
-}
